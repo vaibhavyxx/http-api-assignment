d0ca538411a8c3146939dc056e4482e19651db1b
vaibhavyxx <vd5443@rit.edu>
2026-02-08 20:37:17 -0500

    xml does not work

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,57 +1,60 @@
-const { request } = require("http");
-
 //sends a json
-const respond = (request, response, status, object, isJSON) => {
-    let content;
-    if (isJSON) {
-        content = JSON.stringify(object);
-    } else {
-        content = object; // already a string (XML, text, etc.)
-    }
+const getResponseType = (response) => {
+  const accept = response.headers.get('accept');
+
+  if (accept && accept.includes('text/xml')) {
+    return 'xml';
+  }
+
+  return 'json'; // default
+};
+
+const respond = (request, response, status, content) => {
     const headers = {
-    'Content-Type': isJSON ? 'application/json' : 'text/xml',
+    'Content-Type': 'application/json',
     'Content-Length': Buffer.byteLength(content, 'utf8'),
     };
     response.writeHead(status, headers);
     response.write(content);
     response.end();
-    //console.log(content);
 };
+
+//helper function for writing messages
+function respondMessage(msg, response) {
+    //const type = getResponseType(response);
+    /*if (type === 'xml') {
+        return `<response><message>${msg}</message></response>`;
+    }*/
+    return JSON.stringify({ message: msg });
+}
 
 //for a success status code
 const success = (request, response) => {
-    const responseJSON = {
-        message: 'This is a successful response',
-    };
-    respond(request, response, 200, responseJSON, true);
+    //const type = getResponseType(response);
+    const body = respondMessage('This is a successful response', request);
+
+    respond(request, response, 200, body);
+    //respond(request, response, 200, respondMessage('This is a successful response', response));
 };
 
-const responseXML= (request, response, responseJSON) => {
-    if(request.acceptedTypes[0] === 'text/xml'){
-        let responseXML = `<response>${responseJSON.message}</response>`
-        return respond(request, response, 200, responseXML, false);
-    }
-}
-
 const badRequest = (request, response) => {
-    const responseJSON = {
-        message: 'This request had the required parameters',
-    };
+    //const type = getResponseType(response);
 
-    if(!request.query.valid || request.query.valid !== 'true'){
-        responseJSON.message = 'Missing valid query parameters';
-        responseJSON.id = 'badRequest';
-        return respond(request, response, 400, responseJSON, true);
-    }
-    return respond(request, response, 200, responseJSON, true);
+  if (!request.query.valid || request.query.valid !== 'true') {
+    const body = respondMessage('Missing valid query parameters', request);
+    return respond(request, response, 400, body);
+  }
+
+  const body = respondMessage('This request had the required parameters', request);
+  return respond(request, response, 200, body);
+//return respond(request, response, 200, res);
 };
 
 const notFound = (request, response) => {
-    const responseJSON = {
-        message: 'The page you are looking for was not found',
-        id: 'notFound',
-    };
-    respond(request, response, 404,responseJSON, true);
+    //const type = getResponseType(response);
+    const body = respondMessage('The page you are looking for was not found', request);
+
+    respond(request, response, 404, body);
 }
 
 module.exports = {success, badRequest, notFound,};
